1b6fe69b79a2b38edfee60f9b9f483cd9939d3d3
jayesh nair <compskian@gmail.com>
2020-09-20 01:51:57 +0530

    Test for context load

--- src/test/java/com/enggstream/sample/SampleApplicationTests.java
+++ src/test/java/com/enggstream/sample/SampleApplicationTests.java
@@ -1,13 +1,20 @@
 package com.enggstream.sample;
 
+import com.enggstream.sample.controller.EvaluationController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SampleApplicationTests {
 
+	@Autowired
+	private EvaluationController controller;
+
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
