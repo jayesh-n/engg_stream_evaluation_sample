ce059e44bd85a2f421e3092a5de0a7e876d374c1
jayesh nair <compskian@gmail.com>
2020-09-20 01:40:00 +0530

    Test for controller

---
+++ src/test/java/com/enggstream/sample/controller/evaluationControllerTests.java
@@ -0,0 +1,27 @@
+package com.enggstream.sample.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest
+public class evaluationControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getMessages() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello welcome"));
+    }
+}
